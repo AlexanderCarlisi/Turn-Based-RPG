8dbecd1ebd15002299accc2a9e3421cb643f6fef
Alexander Carlisi <alexandercarlisi0@gmail.com>
2024-04-25 12:30:42 -0400

    Add GameManagerScript and related files

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    private static Player player;
+
+    // Start is called before the first frame update
+    void Start() {
+        player = new Player();
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
