46775db692f0b4ccbe13b09ba6aeef941d66001d
Alexander Carlisi <alexandercarlisi0@gmail.com>
2024-04-28 13:17:58 -0400

    Refactor Enemy class in Units.cs to improve battle action algorithm

--- Assets/Scripts/Classes/Units.cs
+++ Assets/Scripts/Classes/Units.cs
@@ -247,4 +247,135 @@ public class Enemy : Unit {
     public Enums.AlgorithemType getAlgorithem() {
         return algorithem;
     }
+
+    public BattleAction getAction(PartyMember[] party, Enemy[] enemies) {
+        Unit target = party[0];
+        Skill skill = null;
+        Enums.BattleAction actionType = Enums.BattleAction.Weapon;
+
+
+        switch (algorithem) {
+            case Enums.AlgorithemType.Attacker: {
+                // check for buffs and buff skill, if not buffed buff themselves
+                // else attack a random party member
+
+                // Get Random Target
+                target = party[UnityEngine.Random.Range(0, party.Length)];
+
+                // Check if it should use a Skill or it's Weapon
+                bool useSkill = UnityEngine.Random.Range(0, 1) == 1;
+                if (useSkill) {
+                    skill = getSkills()[UnityEngine.Random.Range(0, getSkills().Length)];
+                    actionType = Enums.BattleAction.Skill;
+
+                    // Check if the cost is affordable, if not use recursion and try again
+                    if (!checkCost(skill.getType(), skill.getCost())) {
+                        return getAction(party, enemies);
+                    }
+
+                } else { // Use the Weapon
+                    actionType = Enums.BattleAction.Weapon;
+                }
+                break;
+            }
+                
+            case Enums.AlgorithemType.Support: {
+                // check if any teammate is at or below 30% hp, if so heal them
+                // else buff a random teammate that isn't already buffed.
+
+                // Get first Healing Skill that is affordable
+                foreach (Skill sk in getSkills()) {
+                    if (sk is HealSkill && checkCost(skill.getType(), skill.getCost())) {
+                        // Get for first teammate that is at or below 30% hp
+                        foreach (Enemy enemy in enemies) {
+                            if (enemy.getHp() <= enemy.getMaxHp() * 0.3) {
+                                skill = sk;
+                                actionType = Enums.BattleAction.Skill;
+                                target = enemy;
+                                return new BattleAction(actionType, skill, target);
+                            }
+                        }
+                        break; // No target to heal
+                    }
+                    // make an array of skills that are buffs/debuffs
+                }
+                break;
+            }
+                
+            case Enums.AlgorithemType.Tank: {
+                // Attack opponent with the highest current Hp
+                // Use the skill that does the most damage and within cost
+
+                // Get the opponent with the highest current Hp
+                foreach (PartyMember member in party) {
+                    if (member.getHp() > target.getHp()) {
+                        target = member;
+                    }
+                }
+
+                // Get the skill that does the most damage and within cost
+                foreach (Skill sk in getSkills()) {
+                    if (skill == null && sk is AttackSkill atkSkil && checkCost(sk.getType(), sk.getCost())) {
+                        skill = atkSkil;
+                        actionType = Enums.BattleAction.Skill;
+                    }
+                    else if (sk is AttackSkill atkSkill && skill is AttackSkill topSkill && checkCost(sk.getType(), sk.getCost())) {
+                        if (skill == null || atkSkill.getBaseDamage() > topSkill.getBaseDamage()) {
+                            skill = atkSkill;
+                        }
+                    }
+                }
+
+                // If there is no Attack skills wihin cost
+                if (skill == null) actionType = Enums.BattleAction.Weapon;
+                break;
+            }
+                
+            case Enums.AlgorithemType.Sniper: {
+                // Attack opponent with the lowest current Hp
+                // If the opponent is dead, get the next lowest
+                foreach (PartyMember member in party) {
+                    if (member.getHp() < target.getHp() && member.isAlive()) {
+                        target = member;
+                    }
+                }
+
+                // If the target is at 10% or less hp, check each attackSkill for one that will kill
+                AttackSkill strongestSkill = null;
+                if (target.getHp() <= target.getMaxHp() * 0.1) foreach (Skill sk in getSkills()) {
+                    // Look for attack skill within cost
+                    if (sk is AttackSkill atkSkill && checkCost(sk.getType(), sk.getCost())) {
+                        // Keep track of Strongest Skill.
+                        if (strongestSkill == null || atkSkill.getBaseDamage() > strongestSkill.getBaseDamage()) {
+                            strongestSkill = atkSkill;
+                        }
+
+                        // Simulate damage to the target and check if it will die, DOESN'T necessarily mean they WILL die.
+                        int damage = BattleHandlerScript.damageCalc(atkSkill, this, target);
+                        // If multiple skills will kill, use the one with the lowest cost.
+                        if (target.getHp() - damage <= 0 && atkSkill.getCost() < skill.getCost()) {
+                            skill = atkSkill;
+                            actionType = Enums.BattleAction.Skill;
+                            break;
+                        }
+                    }
+                }
+
+                // If no skill will kill, use the strongest skill.
+                if (skill == null && strongestSkill != null) {
+                    skill = strongestSkill;
+                    actionType = Enums.BattleAction.Skill;
+                } else { // If there are no AttackSkills within Cost use Weapon
+                    actionType = Enums.BattleAction.Weapon;
+                }
+
+                break;
+            }
+                
+            // case Enums.AlgorithemType.TeamPlayer:
+            // Requires implementation of BattleTurn history 
+        }
+
+        return new BattleAction(actionType, skill, target);
+    }
 }
